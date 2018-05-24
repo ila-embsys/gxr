d59b809fece6bb8becdf3c91364aee8d932b151a
Lubosz Sarnecki <lubosz.sarnecki@collabora.com>
2018-05-24 10:49:18 +0200

    examples: add cogl-2.0 context pixbuf example.

---
+++ examples/overlay_pixbuf_cogl.c
@@ -0,0 +1,228 @@
+/*
+ * OpenVR GLib
+ * Copyright 2018 Lubosz Sarnecki <lubosz.sarnecki@collabora.com>
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <glib.h>
+#include <gdk-pixbuf/gdk-pixbuf.h>
+#include <gdk/gdk.h>
+
+#include <openvr-glib.h>
+
+#include "openvr-system.h"
+#include "openvr-overlay.h"
+
+#include <GL/gl.h>
+
+#define COGL_ENABLE_EXPERIMENTAL_API 1
+#include <cogl/cogl.h>
+#include <cogl/cogl-sdl.h>
+
+gboolean
+timeout_callback (gpointer data)
+{
+  OpenVROverlay *overlay = (OpenVROverlay*) data;
+  openvr_overlay_poll_event (overlay);
+  return TRUE;
+}
+
+void
+print_pixbuf_info (GdkPixbuf * pixbuf)
+{
+  gint n_channels = gdk_pixbuf_get_n_channels (pixbuf);
+
+  GdkColorspace colorspace = gdk_pixbuf_get_colorspace (pixbuf);
+
+  gint bits_per_sample = gdk_pixbuf_get_bits_per_sample (pixbuf);
+  gboolean has_alpha = gdk_pixbuf_get_has_alpha (pixbuf);
+
+  gint width = gdk_pixbuf_get_width (pixbuf);
+  gint height = gdk_pixbuf_get_height (pixbuf);
+
+  gint rowstride = gdk_pixbuf_get_rowstride (pixbuf);
+
+  g_print ("We loaded a pixbuf.\n");
+  g_print ("channels %d\n", n_channels);
+  g_print ("colorspace %d\n", colorspace);
+  g_print ("bits_per_sample %d\n", bits_per_sample);
+  g_print ("has_alpha %d\n", has_alpha);
+  g_print ("pixel dimensions %dx%d\n", width, height);
+  g_print ("rowstride %d\n", rowstride);
+}
+
+void
+print_gl_context_info ()
+{
+  const GLubyte *version = glGetString (GL_VERSION);
+  const GLubyte *vendor = glGetString (GL_VENDOR);
+  const GLubyte *renderer = glGetString (GL_RENDERER);
+
+  GLint major;
+  GLint minor;
+  glGetIntegerv (GL_MAJOR_VERSION, &major);
+  glGetIntegerv (GL_MINOR_VERSION, &minor);
+
+  g_print ("%s %s %s (%d.%d)\n", vendor, renderer, version, major, minor);
+}
+
+GdkPixbuf *
+load_gdk_pixbuf ()
+{
+  GError *error = NULL;
+  GdkPixbuf * pixbuf_unflipped =
+    gdk_pixbuf_new_from_resource ("/res/cat.jpg", &error);
+
+  if (error != NULL) {
+    fprintf (stderr, "Unable to read file: %s\n", error->message);
+    g_error_free (error);
+    return NULL;
+  } else {
+    GdkPixbuf * pixbuf = gdk_pixbuf_flip (pixbuf_unflipped, FALSE);
+    g_object_unref (pixbuf_unflipped);
+    return pixbuf;
+  }
+}
+
+gboolean
+init_offscreen_gl ()
+{
+  CoglError *error = NULL;
+  //CoglContext *ctx = cogl_context_new (NULL, &error);
+
+  CoglContext *ctx = cogl_sdl_context_new (SDL_USEREVENT, &error);
+
+  if (!ctx)
+    {
+      fprintf (stderr, "Failed to create context: %s\n", error->message);
+      return FALSE;
+    }
+
+  // CoglOffscreen
+  CoglOnscreen *onscreen;
+  onscreen = cogl_onscreen_new (ctx, 800, 600);
+  cogl_onscreen_show (onscreen);
+
+  return ctx != NULL;
+}
+
+static void
+_move_cb (OpenVROverlay  *overlay,
+          GdkEventMotion *event,
+          gpointer        data)
+{
+  g_print ("move: %f %f (%d)\n", event->x, event->y, event->time);
+}
+
+static void
+_press_cb (OpenVROverlay  *overlay,
+           GdkEventButton *event,
+           gpointer        data)
+{
+  g_print ("press: %d %f %f (%d)\n",
+           event->button, event->x, event->y, event->time);
+  GMainLoop *loop = (GMainLoop*) data;
+  g_main_loop_quit (loop);
+}
+
+static void
+_release_cb (OpenVROverlay  *overlay,
+             GdkEventButton *event,
+             gpointer        data)
+{
+  g_print ("release: %d %f %f (%d)\n",
+           event->button, event->x, event->y, event->time);
+}
+
+static void
+_show_cb (OpenVROverlay *overlay,
+          gpointer       data)
+{
+  g_print ("show\n");
+
+  /* skip rendering if the overlay isn't available or visible */
+  gboolean is_unavailable = !openvr_overlay_is_valid (overlay) ||
+                            !openvr_system_is_overlay_available ();
+  gboolean is_invisible = !openvr_overlay_is_visible (overlay) &&
+                          !openvr_overlay_thumbnail_is_visible (overlay);
+
+  if (is_unavailable || is_invisible)
+    return;
+
+  GdkPixbuf * pixbuf = (GdkPixbuf*) data;
+  openvr_overlay_upload_gdk_pixbuf (overlay, pixbuf);
+}
+
+static void
+_destroy_cb (OpenVROverlay *overlay,
+             gpointer       data)
+{
+  g_print ("destroy\n");
+  GMainLoop *loop = (GMainLoop*) data;
+  g_main_loop_quit (loop);
+}
+
+int
+test_cat_overlay ()
+{
+  GMainLoop *loop;
+
+  GdkPixbuf * pixbuf = load_gdk_pixbuf ();
+  print_pixbuf_info (pixbuf);
+
+  if (pixbuf == NULL)
+    return -1;
+
+  loop = g_main_loop_new (NULL, FALSE);
+
+  if (!init_offscreen_gl ())
+  {
+    fprintf (stderr, "Error: Could not create COGL context.\n");
+    return FALSE;
+  }
+
+  print_gl_context_info ();
+
+  OpenVRSystem * system = openvr_system_new ();
+  gboolean ret = openvr_system_init_overlay (system);
+
+  g_assert (ret);
+  g_assert (openvr_system_is_available ());
+  g_assert (openvr_system_is_compositor_available ());
+
+  OpenVROverlay *overlay = openvr_overlay_new ();
+  openvr_overlay_create (overlay, "test.cat", "Cat");
+
+  if (!openvr_overlay_is_valid (overlay) ||
+      !openvr_system_is_overlay_available ())
+  {
+    fprintf (stderr, "Overlay unavailable.\n");
+    return -1;
+  }
+
+  openvr_overlay_set_mouse_scale (overlay,
+                                  (float) gdk_pixbuf_get_width (pixbuf),
+                                  (float) gdk_pixbuf_get_height (pixbuf));
+
+  g_signal_connect (overlay, "motion-notify-event", (GCallback) _move_cb, NULL);
+  g_signal_connect (overlay, "button-press-event", (GCallback) _press_cb, loop);
+  g_signal_connect (
+    overlay, "button-release-event", (GCallback) _release_cb, NULL);
+  g_signal_connect (overlay, "show", (GCallback) _show_cb, pixbuf);
+  g_signal_connect (overlay, "destroy", (GCallback) _destroy_cb, loop);
+
+  g_timeout_add (20, timeout_callback, overlay);
+  g_main_loop_run (loop);
+  g_main_loop_unref (loop);
+
+  g_print ("bye\n");
+
+  g_object_unref (overlay);
+  g_object_unref (pixbuf);
+
+  return 0;
+}
+
+int main (int argc, char *argv[]) {
+  return test_cat_overlay ();
+}
