132f8108f5d71a05586b0a475d2db66374611ff1
Lubosz Sarnecki <lubosz.sarnecki@collabora.com>
2018-08-16 13:46:47 +0200

    Remove OpenGL and cogl.

--- src/openvr-overlay.h
+++ src/openvr-overlay.h
@@ -8,9 +8,6 @@
 #ifndef OPENVR_GLIB_OVERLAY_H_
 #define OPENVR_GLIB_OVERLAY_H_
 
-#define COGL_ENABLE_EXPERIMENTAL_API 1
-#include <cogl/cogl.h>
-
 #include <stdint.h>
 #include <glib-object.h>
 #include <gdk-pixbuf/gdk-pixbuf.h>
@@ -43,56 +40,17 @@ openvr_overlay_create_for_dashboard (OpenVROverlay *self,
                                      gchar* key,
                                      gchar* name);
 
-void openvr_overlay_upload_gdk_pixbuf (OpenVROverlay *self,
-                                       GdkPixbuf * pixbuf);
-
-void openvr_overlay_upload_cairo_surface (OpenVROverlay *self,
-                                          cairo_surface_t* surface);
-
-
-void
-openvr_overlay_upload_cogl (OpenVROverlay *self, CoglTexture *cogl_texture);
-
 void openvr_overlay_poll_event (OpenVROverlay *self);
 
 void openvr_overlay_set_mouse_scale (OpenVROverlay *self,
                                      float width,
                                      float height);
 
-void
-openvr_overlay_upload_pixels (OpenVROverlay *self,
-                              guchar *pixels,
-                              int width,
-                              int height,
-                              unsigned gl_format);
-
-void
-openvr_overlay_set_raw (OpenVROverlay *self, guchar *pixels,
-                        uint32_t width, uint32_t height, uint32_t depth);
-
-void
-openvr_overlay_set_cairo_surface_raw (OpenVROverlay *self,
-                                      cairo_surface_t* surface);
-
-void
-openvr_overlay_set_gl_texture (OpenVROverlay *self, unsigned tex);
-
 gboolean openvr_overlay_is_valid (OpenVROverlay *self);
 gboolean openvr_overlay_is_visible (OpenVROverlay *self);
 gboolean openvr_overlay_thumbnail_is_visible (OpenVROverlay *self);
 
 gboolean openvr_overlay_is_available (OpenVROverlay * self);
-
-void
-openvr_overlay_set_gdk_pixbuf_raw (OpenVROverlay *self,
-                                   GdkPixbuf * pixbuf);
-
-void
-openvr_overlay_upload_pixels_cogl (OpenVROverlay *self,
-                                   guchar *pixels,
-                                   int width,
-                                   int height,
-                                   unsigned gl_format);
 
 G_END_DECLS
 
