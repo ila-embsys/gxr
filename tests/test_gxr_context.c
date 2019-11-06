70f24e611d07dc3aec16cb00caed2ea09e4af6a0
Lubosz Sarnecki <lubosz.sarnecki@collabora.com>
2019-11-06 16:42:47 +0100

    tests: Add gxr context test.

---
+++ tests/test_gxr_context.c
@@ -0,0 +1,44 @@
+/*
+ * gxr
+ * Copyright 2018 Collabora Ltd.
+ * Author: Lubosz Sarnecki <lubosz.sarnecki@collabora.com>
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <glib.h>
+#include <gulkan.h>
+#include "gxr.h"
+
+#define ENUM_TO_STR(r) case r: return #r
+
+static const gchar*
+gxr_api_string (GxrApi v)
+{
+  switch (v)
+    {
+      ENUM_TO_STR(GXR_API_OPENXR);
+      ENUM_TO_STR(GXR_API_OPENVR);
+      default:
+        return "UNKNOWN API";
+    }
+}
+
+static void
+_test_minimal ()
+{
+  GxrContext *context = gxr_context_new ();
+  g_assert_nonnull (context);
+
+  GxrApi api = gxr_context_get_api (context);
+  g_print ("Using API: %s\n", gxr_api_string (api));
+
+  g_object_unref (context);
+}
+
+int
+main ()
+{
+  _test_minimal ();
+
+  return 0;
+}
