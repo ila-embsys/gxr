ba33591dd21f1cbbc11d71157db30fee827c0d6a
Lubosz Sarnecki <lubosz.sarnecki@collabora.com>
2019-11-29 14:03:33 +0100

    tests: Create combined context test.

--- tests/test_gxr_context.c
+++ tests/test_gxr_context.c
@@ -23,6 +23,44 @@ gxr_api_string (GxrApi v)
     }
 }
 
+#ifdef GXR_HAS_OPENXR
+static void
+_init_openxr (OpenXRContext *context)
+{
+  GulkanClient *gc = gulkan_client_new ();
+  g_assert (gulkan_client_init_vulkan (gc, NULL, NULL));
+
+  GulkanInstance *gk_instance = gulkan_client_get_instance (gc);
+  VkInstance vk_instance = gulkan_instance_get_handle (gk_instance);
+
+  GulkanDevice *gk_device = gulkan_client_get_device (gc);
+  VkDevice vk_device = gulkan_device_get_handle (gk_device);
+
+  VkPhysicalDevice physical_device =
+    gulkan_device_get_physical_handle (gk_device);
+
+  uint32_t queue_family_index = gulkan_device_get_queue_family_index (gk_device);
+
+  uint32_t queue_index = 0;
+
+  g_assert (openxr_context_initialize (context, vk_instance,
+                                       physical_device,
+                                       vk_device,
+                                       queue_family_index,
+                                       queue_index));
+}
+#endif
+
+#ifdef GXR_HAS_OPENVR
+static void
+_init_openvr (OpenVRContext *context)
+{
+  g_assert (openvr_context_initialize (context, GXR_APP_OVERLAY));
+  g_assert (openvr_context_is_valid (context));
+  g_object_unref (context);
+}
+#endif
+
 static void
 _test_minimal ()
 {
@@ -34,10 +72,20 @@ _test_minimal ()
 
   switch (api)
     {
+#ifdef GXR_HAS_OPENVR
       case GXR_API_OPENVR:
+        g_print ("Initializing OpenVR context.");
+        _init_openvr (OPENVR_CONTEXT (context));
         break;
+#endif
+#ifdef GXR_HAS_OPENXR
       case GXR_API_OPENXR:
+        g_print ("Initializing OpenXR context.");
+        _init_openxr (OPENXR_CONTEXT (context));
         break;
+#endif
+      default:
+        g_printerr ("Unsupported XR API selected.\n");
     }
 
   g_object_unref (context);
