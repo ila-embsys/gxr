65f798a259a7e39c227884155dea13e0d8dd2251
Lubosz Sarnecki <lubosz.sarnecki@collabora.com>
2020-01-08 20:02:50 +0100

    openxr: Add OpenXROverlay stub class.

---
+++ src/openxr/openxr-overlay.h
@@ -0,0 +1,26 @@
+/*
+ * gxr
+ * Copyright 2020 Collabora Ltd.
+ * Author: Lubosz Sarnecki <lubosz.sarnecki@collabora.com>
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef GXR_OPENXR_OVERLAY_H_
+#define GXR_OPENXR_OVERLAY_H_
+
+#if !defined (GXR_INSIDE) && !defined (GXR_COMPILATION)
+#error "Only <gxr.h> can be included directly."
+#endif
+
+#include "gxr-overlay.h"
+
+G_BEGIN_DECLS
+
+#define OPENXR_TYPE_OVERLAY openxr_overlay_get_type()
+G_DECLARE_FINAL_TYPE (OpenXROverlay, openxr_overlay, OPENXR, OVERLAY, GxrOverlay)
+
+OpenXROverlay *openxr_overlay_new (void);
+
+G_END_DECLS
+
+#endif /* GXR_OPENXR_OVERLAY_H_ */
