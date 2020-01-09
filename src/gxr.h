fc38d797cab557c506bb77f8281720aec14fe384
Lubosz Sarnecki <lubosz.sarnecki@collabora.com>
2020-01-09 01:11:59 +0100

    gxr.h: Don't expose backend headers.

--- src/gxr.h
+++ src/gxr.h
@@ -26,22 +26,6 @@
 #include "gxr-overlay.h"
 #include "gxr-overlay-model.h"
 
-#ifdef GXR_HAS_OPENVR
-  #include "openvr-action.h"
-  #include "openvr-action-set.h"
-  #include "openvr-compositor.h"
-  #include "openvr-context.h"
-  #include "openvr-overlay.h"
-  #include "openvr-system.h"
-  #include "openvr-model.h"
-#endif
-
-#ifdef GXR_HAS_OPENXR
-  #include "openxr-context.h"
-  #include "openxr-action-set.h"
-  #include "openxr-action.h"
-#endif
-
 #undef GXR_INSIDE
 
 #endif
